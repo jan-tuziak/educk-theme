081d589278acdcf5ff85e2525683bc2774b859c2
Jan Tuziak <48406465+jan-tuziak@users.noreply.github.com>
2025-02-19 19:19:59 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,9 +5,10 @@ include_once get_theme_file_path($include_file);
 include_once get_theme_file_path('include/wp-login-modified.php');
 
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles');
-function enqueue_parent_styles() {
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 }
 
 /**
