1e26f153197835d4a87946a9168e3f76337032b2
Jan Tuziak <jan.tuziak@gmail.com>
2025-03-07 15:06:01 +0100

    first version

---
+++ include/woo.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ *  Change Main Woocommerce Button for Courses to "Read More"
+ */
+add_filter( 'woocommerce_loop_add_to_cart_link', 'replace_loop_add_to_cart_button', 10, 2 );
+function replace_loop_add_to_cart_button( $button, $product  ) {
+    // Course products
+    if( $product->is_type( 'course' ) ) {
+        $button_text = __( EDUCK_READ_MORE, "woocommerce" );
+		return '<a class="view-product button" href="' . $product->get_permalink() . '">' . $button_text . '</a>';
+    } 
+    // Other product types
+    else {
+		//$button_text = add_to_cart_text(); <- does not work for some reason. Investigate later 
+		$button_text = __( EDUCK_ADD_TO_CART, "woocommerce" );
+		return '<a class="view-product button" href="?add-to-cart=' . $product->get_id() . '">' . $button_text . '</a>';
+    }
+}
+
+/**
+ * @snippet       Prices Incl + Excl Tax | WooCommerce Shop
+ * @how-to        businessbloomer.com/woocommerce-customization
+ * @author        Rodolfo Melogli, Business Bloomer
+ * @compatible    WooCommerce 7
+ * @community     https://businessbloomer.com/club/
+ */
+add_filter( 'woocommerce_get_price_suffix', 'bbloomer_add_price_suffix_price_inc_tax', 99, 4 );
+function bbloomer_add_price_suffix_price_inc_tax( $suffix, $product, $price, $qty ){
+    $suffix = '<small> ' . wc_price( wc_get_price_excluding_tax( $product ) ) . ' ' . EDUCK_NET .'</small>';
+    return $suffix;
+}
