74c0dae54a73e07ea9e4f8244c4058ef3e5462ba
Jan Tuziak <48406465+jan-tuziak@users.noreply.github.com>
2025-03-30 21:08:37 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,8 +9,11 @@ include_once get_theme_file_path('include/elementor-form-turnstile-handler.php')
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+	//Skipping parent style, because it was messing up headings on Blog posts
+	//wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+        //wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+	
+        wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css');
 }
 
 /**
