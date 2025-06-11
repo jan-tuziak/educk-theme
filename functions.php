f980985844883333df75894ff9a8a1e2ba9f4ec0
Jan Tuziak <48406465+jan-tuziak@users.noreply.github.com>
2025-06-11 22:08:43 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -65,24 +65,6 @@ function enqueue_learndash_styles(){
 add_action( 'wp_enqueue_scripts', 'enqueue_learndash_styles' );
 
 /**
- * Enqueue Script for Allowing All Consent for Google Analytics
- **/
-add_action( 'wp_enqueue_scripts', 'allow_all_consent' );
-function allow_all_consent(){
-	wp_register_script( 'allow-all-consent', '', [], '', true );
-	wp_enqueue_script( 'allow-all-consent'  );
-	wp_add_inline_script( 'allow-all-consent', 
-		'
-		gtag("consent", "update", {
-			"ad_user_data": "granted",
-			"ad_personalization": "granted",
-			"ad_storage": "granted",
-			"analytics_storage": "granted"
-		  });
-		' );
-}
-
-/**
  * @snippet       Remove Tax if Field Value - WooCommerce Checkout
  * @how-to        businessbloomer.com/woocommerce-customization
  * @author        Rodolfo Melogli, Business Bloomer, BusinessBloomer.com
