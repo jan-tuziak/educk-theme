6ae8ada25180757edcd9051f2900bad3b3af42ea
Jan Tuziak <48406465+jan-tuziak@users.noreply.github.com>
2025-06-11 22:08:58 +0200

    Update org.php

--- include/org.php
+++ include/org.php
@@ -2,3 +2,21 @@
 define('EDUCK_READ_MORE', "Read More");
 define('EDUCK_ADD_TO_CART', "Add to Cart");
 define('EDUCK_NET', "excl. VAT");
+
+/**
+ * Enqueue Script for Allowing All Consent for Google Analytics
+ **/
+add_action( 'wp_enqueue_scripts', 'allow_all_consent' );
+function allow_all_consent(){
+	wp_register_script( 'allow-all-consent', '', [], '', true );
+	wp_enqueue_script( 'allow-all-consent'  );
+	wp_add_inline_script( 'allow-all-consent', 
+		'
+		gtag("consent", "update", {
+			"ad_user_data": "granted",
+			"ad_personalization": "granted",
+			"ad_storage": "granted",
+			"analytics_storage": "granted"
+		  });
+		' );
+}
