e6042e49ab402ad64aa6f264f5c45e50e8a193fc
Jan Tuziak <48406465+jan-tuziak@users.noreply.github.com>
2025-07-02 16:01:04 +0200

    up

--- include/woo.php
+++ include/woo.php
@@ -115,21 +115,21 @@ function bbloomer_display_checkbox_and_new_checkout_field( $fields ) {
     $fields['billing']['billing_first_name']['priority'] = 2;
     $fields['billing']['billing_last_name']['priority'] = 3;
     $fields['billing']['billing_country']['priority'] = 4;
-    $fields['billing']['billing_postcode']['priority'] = 5;
     
-    $fields['billing']['checkbox_vat_invoice']['priority'] = 6;
-    $fields['billing']['billing_tax_no']['priority'] = 7;
-    $fields['billing']['billing_company']['priority'] = 8;
-    $fields['billing']['billing_address_1']['priority'] = 9;
+    $fields['billing']['checkbox_vat_invoice']['priority'] = 5;
+    $fields['billing']['billing_tax_no']['priority'] = 6;
+    $fields['billing']['billing_company']['priority'] = 7;
+    $fields['billing']['billing_address_1']['priority'] = 8;
+    $fields['billing']['billing_postcode']['priority'] = 9;
     $fields['billing']['billing_city']['priority'] = 10;
 	return $fields;
 }
 
-add_filter( 'woocommerce_default_address_fields', 'custom_override_default_locale_fields' );
-function custom_override_default_locale_fields( $fields ) {
-    $fields['address_1']['priority'] = 9;
-    return $fields;
-}
+// add_filter( 'woocommerce_default_address_fields', 'custom_override_default_locale_fields' );
+// function custom_override_default_locale_fields( $fields ) {
+//     $fields['address_1']['priority'] = 8;
+//     return $fields;
+// }
   
 add_action( 'woocommerce_after_checkout_form', 'bbloomer_conditionally_hide_show_new_field', 9999 );
 function bbloomer_conditionally_hide_show_new_field() {
