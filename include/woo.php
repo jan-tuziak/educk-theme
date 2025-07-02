fe3c5a55c1ddb8ffa7f8fdd7db0c774eaefb2872
Jan Tuziak <48406465+jan-tuziak@users.noreply.github.com>
2025-07-02 12:23:50 +0200

    Update woo.php

--- include/woo.php
+++ include/woo.php
@@ -31,6 +31,53 @@ function bbloomer_add_price_suffix_price_inc_tax( $suffix, $product, $price, $qt
 }
 
 
+
+
+
+
+
+
+
+/**
+ * @snippet       Hide Fields if Virtual @ WooCommerce Checkout
+ * @how-to        businessbloomer.com/woocommerce-customization
+ * @author        Rodolfo Melogli, Business Bloomer
+ * @compatible    WooCommerce 8
+ * @community     https://businessbloomer.com/club/
+ */
+ 
+add_filter( 'woocommerce_checkout_fields', 'bbloomer_simplify_checkout_virtual' );
+  
+function bbloomer_simplify_checkout_virtual( $fields ) {
+   $only_virtual = true;
+   foreach( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+      // Check if there are non-virtual products
+      if ( ! $cart_item['data']->is_virtual() ) $only_virtual = false;
+   }
+   if ( $only_virtual ) {
+      unset($fields['billing']['billing_company']);
+      unset($fields['billing']['billing_address_1']);
+      unset($fields['billing']['billing_address_2']);
+      unset($fields['billing']['billing_city']);
+      // unset($fields['billing']['billing_postcode']);
+      // unset($fields['billing']['billing_country']);
+      unset($fields['billing']['billing_state']);
+      unset($fields['billing']['billing_phone']);
+      // add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+   }
+   return $fields;
+}
+
+
+
+
+
+
+
+
+
+
+
 // /**
 //  * @snippet       Add a Checkbox to Hide/Show Checkout Field - WooCommerce
 //  * @how-to        businessbloomer.com/woocommerce-customization
